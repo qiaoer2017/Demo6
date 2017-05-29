c5b190fdb1ec656eef79b96eec7793a44c0ffb48
qiaoer <qiaoer2017@163.com>
2017-05-29 18:40:55 +0800

    上一个，下一个按钮事件

--- js/script.js
+++ js/script.js
@@ -5,14 +5,52 @@
     var main = byId('main');
     var pics = byId('banner').getElementsByTagName('div');
     var indicators = byId('dots').getElementsByTagName('span');
+    var prev = byId('prev');
+    var next = byId('next');
+
     var len = pics.length;
+
+
     slideImg();
     main.onmouseout();
+
+    //上一张
+    prev.onclick = function () {
+        index--;
+        if (index <= -1) {
+            index = len - 1;
+        }
+        changeImg();
+    };
+    //下一个
+    next.onclick = function () {
+        index++;
+        if (index >= len) {
+            index = 0;
+        }
+        changeImg();
+    };
+
+    for (var i = 0; i < len; i++) {
+        indicators[i].setAttribute('data-index', i);
+        indicators[i].onclick = function () {
+            var indicatorIndex = this.getAttribute('data-index');
+            if (index === parseInt(indicatorIndex)) {
+                return;
+            }
+            //改变index为当前索引
+            index = indicatorIndex;
+            changeImg();
+        };
+    }
+
 
     function slideImg() {
         //清除定时器
         main.onmouseover = function () {
-            clearInterval(timer);
+            if (timer) {
+                clearInterval(timer);
+            }
         };
 
         //继续
@@ -23,18 +61,18 @@
                 //切换图片
                 changeImg();
 
-            }, 1000);
+            }, 3000);
         };
     }
 
     function changeImg() {
-        console.log(index);
+        // console.log(index);
         for (var i = 0; i < len; i++) {
             pics[i].style.display = 'none';
-            indicators[i].style.backgroundColor = 'transparent';
+            indicators[i].className = '';
         }
         pics[index].style.display = 'block';
-        indicators[index].style.backgroundColor = '#fff';
+        indicators[index].className = 'active';
     }
 
     function byId(id) {
